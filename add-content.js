133aa3fe9d66d3c3efb0870f7fb83230afe242c3
Kaigf <kai@elitetopro.com>
2017-03-09 08:54:17 -0800

    Add files via upload

---
+++ add-content.js
@@ -0,0 +1,9 @@
+/* Javascript */
+//script for registration
+function GetFormData() {
+	var name=document.getElementById('gamertag').value;
+	var name=document.getElementById('email').value;
+	var name=document.getElementById('password').value;
+	var name=document.getElementById('conf-password').value;
+	CreateUserWithName(gamertag,email,password,conf-password);	
+}
